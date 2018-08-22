31bcf397747044d8b445f40597937600ae4f0219
Rene Vallecillo Vega <reneval@gmail.com>
2018-08-21 18:34:05 -0600

    first implementation left rotator

---
+++ Rotate-Left/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Complete the rotLeft function below.
+func rotLeft(a []int32, d int32) []int32 {
+	for r := 0; r < int(d); r++ {
+		popped := a[0]
+		fmt.Println("popped:", popped)
+		for x := 0; x < len(a); x++ {
+			if x+1 >= len(a) {
+				a[len(a)-1] = popped
+			} else {
+				a[x] = a[x+1]
+			}
+		}
+		fmt.Println("new array:", a)
+
+	}
+
+	return a
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	stdout, err := os.Create("out")
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	nd := strings.Split(readLine(reader), " ")
+
+	nTemp, err := strconv.ParseInt(nd[0], 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	dTemp, err := strconv.ParseInt(nd[1], 10, 64)
+	checkError(err)
+	d := int32(dTemp)
+
+	aTemp := strings.Split(readLine(reader), " ")
+
+	var a []int32
+
+	for i := 0; i < int(n); i++ {
+		aItemTemp, err := strconv.ParseInt(aTemp[i], 10, 64)
+		checkError(err)
+		aItem := int32(aItemTemp)
+		a = append(a, aItem)
+	}
+
+	result := rotLeft(a, d)
+	fmt.Println(result)
+
+	for i, resultItem := range result {
+		fmt.Fprintf(writer, "%d", resultItem)
+
+		if i != len(result)-1 {
+			fmt.Fprintf(writer, " ")
+		}
+	}
+
+	fmt.Fprintf(writer, "\n")
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
